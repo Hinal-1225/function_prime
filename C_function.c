dc8f53ae0cb80294e1ddc55c88f179ba43157b92
hinal-025 <68282568+hinal-025@users.noreply.github.com>
2020-08-21 17:12:46 +0530

    Add files via upload

---
+++ C_function.c
@@ -0,0 +1,32 @@
+#include<stdlib.h>
+#include<stdio.h>
+void fun_prime(int n);
+int main()
+{
+    system("cls");
+    int n;
+    printf("\nEnter number : ");
+    scanf("%d",&n);
+    fun_prime(n);
+    system("pause");
+	return 10;
+}
+void fun_prime(int n)
+{
+    int i;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            printf("\n%d is not a prime number.\n ",n);
+            break;
+        }
+        else
+        {
+            printf("\n%d is a prime number. \n",n);
+            break;
+        }
+}
+}
+
+
